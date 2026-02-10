0561af047ea6da097ffaaa49d04ea5c752a666d3
tikkasaurus <50169047+tikkasaurus@users.noreply.github.com>
2026-02-10 08:25:11 +0100

    remove usage of theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./app/App";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient.ts";
-import { ThemeProvider } from "./lib/theme-context";
 import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
@@ -18,11 +17,9 @@ tokenProvider.setMsalInstance(msalInstance);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MsalProvider instance={msalInstance}>
-      <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </MsalProvider>
   </StrictMode>
 );
